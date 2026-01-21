cb8b52b69f1781e8a92ae7032e99607aad42e46c
Quincy Tennyson <quincytennyson8@gmail.com>
2026-01-21 09:50:48 -0500

    basic app completed with correct length working

--- markov.py
+++ markov.py
@@ -1,15 +1,24 @@
 import random
 import itertools
+from quotes import QUOTES
+
 from collections import defaultdict
 
+## Generate a random set of starter words the user can try.
+def get_random_starter_words(quotes=QUOTES):
+    quotes = random.sample(quotes, k=15)
+    words = [quote.split()[0] for quote in quotes]
+    return list(set(words[:10]))
 
-def build_markov_chains(quotes):
+def build_markov_chains(quotes=QUOTES):
     word_chains = defaultdict(list)
 
     for quote in quotes:
         split_quote = quote.split()
         for index in range(len(split_quote) - 1):
-            word_chains[split_quote[index]].append(split_quote[index+1])
+            word = split_quote[index].strip('.!?').lower()
+            next_word = split_quote[index+1].strip('.!?').lower()
+            word_chains[word].append(next_word)
         
     return word_chains
 
@@ -17,19 +26,21 @@ def build_markov_chains(quotes):
 def generate_text(word_chains, start_word=None, length=10):
 
     def get_backup_word(word_chains):
-        start_words_to_choose_from = itertools.chain.from_iterable(list(word_chains.values()))
+        start_words_to_choose_from = list(itertools.chain.from_iterable(list(word_chains.values())))
         start_word = random.choice(start_words_to_choose_from)
         return start_word
     
-    if not start_word or start_word not in word_chains:
-        start_word = get_backup_word(word_chains)    
+    if start_word is None:
+        start_word = get_backup_word(word_chains) 
+    elif start_word.lower() not in word_chains:
+        start_word = get_backup_word(word_chains) 
 
     new_text = [start_word]
     while len(new_text) < length:
-        if start_word not in word_chains:
+        if start_word.lower() not in word_chains:
             break
-        
-        next_word = random.choice(word_chains.get(start_word))
+
+        next_word = random.choice(word_chains.get(start_word.lower()))
         new_text.append(next_word)
         start_word = next_word
 
--- markov_app.py
+++ markov_app.py
@@ -1,1 +1,20 @@
 import streamlit as st
+from markov import generate_text, build_markov_chains, get_random_starter_words
+
+st.title("Markov Chains with Strings aka Inspirational Quote Generator")
+
+if "random_starter_words" not in st.session_state:
+    st.session_state.random_starter_words = get_random_starter_words()
+
+if "word_chains" not in st.session_state:
+    st.session_state.word_chains = build_markov_chains()
+
+st.write(st.session_state.random_starter_words)
+
+result = st.text_input(
+    label="Enter a starter word from the options above",
+)
+num_words = st.number_input(label="Num. Words to Generate", max_value=20)
+
+if st.button("Generate Text"):
+    st.write(generate_text(st.session_state.word_chains, start_word=result, length=num_words))
